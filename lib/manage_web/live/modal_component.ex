ad985414132faa7576d5d1b11bf71f980818b27f
gallexme <lostchaos3@gmail.com>
2020-04-22 00:54:00 +0200

    Basic Theming

--- lib/manage_web/live/modal_component.ex
+++ lib/manage_web/live/modal_component.ex
@@ -4,7 +4,7 @@ defmodule ManageWeb.ModalComponent do
   @impl true
   def render(assigns) do
     ~L"""
-    <div id="<%= @id %>" class="fixed bottom-0 inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center"
+    <div id="<%= @id %>" class=" fixed bottom-0 inset-x-0 px-4 pt-16 pb-16 sm:inset-0 sm:flex sm:items-center sm:justify-center"
       phx-capture-click="close"
       phx-window-keydown="close"
       phx-key="escape"
@@ -13,8 +13,8 @@ defmodule ManageWeb.ModalComponent do
       <div class="fixed-inset-0 transition-opacity">
         <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
       </div>
-      <div class="phx-modal-content bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full">
-        <%= live_patch raw("&times;"), to: @return_to, class: "phx-modal-close" %>
+      <div class="p-2 py-4   shadow-2xl bg-gray-300 rounded overflow-hidden  transform transition-all sm:max-w-lg sm:w-full">
+        <%= live_patch raw("&times;"), to: @return_to, class: "hover:bg-red-500 bg-red-400 py-2  px-4" %>
         <%= live_component @socket, @component, @opts %>
       </div>
     </div>
